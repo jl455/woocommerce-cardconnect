0d2ad8dd556eab97ea1c88cd9867a3df34e63d53
Eran <me@eran.sh>
2015-05-06 17:24:39 -0400

    Cleanup and placeholder stuff

--- templates/saved-cards.php
+++ templates/saved-cards.php
@@ -1,6 +1,35 @@
-<p class="form-row form-row-wide">
+<p class="form-row form-row-first">
 	<label for="card_connect-save-card">
-		<input id="card_connect-save-card" class="input-checkbox " type="checkbox" name="card_connect-save-card" style="margin-right: 3px"/>
-		<?php echo __( 'Save my Card', 'woocommerce' ); ?>
+		<input
+			id="card_connect-save-card"
+			class="input-checkbox"
+			type="checkbox"
+			name="card_connect-save-card"
+			style="margin-right: 3px"
+			/>
+		<?php echo __( 'Save this card', 'woocommerce' ); ?>
+		<input
+			id="card_connect-new-card-alias"
+			class="input-text"
+			type="text"
+			name="card_connect-new-card-alias"
+		  placeholder="Card Nickname"
+			/>
 	</label>
+</p>
+
+<p class="form-row form-row-last">
+	<label for="card_connect-cards">
+		<?php echo __( 'Use a saved card', 'woocommerce' ); ?>
+	</label>
+	<select
+		id="card_connect-cards"
+		class="input-select"
+		name="card_connect-cards"
+		>
+		<option value="1234">7589</option>
+		<option value="1234">7589</option>
+		<option value="1234">7589</option>
+		<option value="1234">7589</option>
+	</select>
 </p>
